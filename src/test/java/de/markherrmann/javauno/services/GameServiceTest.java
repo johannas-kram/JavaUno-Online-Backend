7671e9b32818d63b90737bfbb42ca39dbe5e138f
tomatenmark <mh88@mailbox.org>
2019-11-09 15:32:12 +0100

    test(unittests): added failure tests to GameServiceTest

--- src/test/java/de/markherrmann/javauno/services/GameServiceTest.java
+++ src/test/java/de/markherrmann/javauno/services/GameServiceTest.java
@@ -1,8 +1,9 @@
 package de.markherrmann.javauno.services;
 
-import de.markherrmann.javauno.data.fixed.Card;
+import java.lang.IllegalArgumentException;
 import de.markherrmann.javauno.data.state.UnoState;
 import de.markherrmann.javauno.data.state.components.Game;
+import de.markherrmann.javauno.data.state.components.GameLifecycle;
 import de.markherrmann.javauno.data.state.components.Player;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +11,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.plugin.dom.exception.InvalidStateException;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,12 +50,77 @@ public class GameServiceTest {
     }
 
     @Test
+    public void shouldFailAddPlayerCausedByInvalidLifecycle(){
+        int playersBefore = game.getPlayer().size();
+        game.setGameLifecycle(GameLifecycle.RUNNING);
+        Exception exception = null;
+
+        try {
+            gameService.addPlayer(game.getUuid(), "player name", false);
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        int playersNow = game.getPlayer().size();
+        assertThat(playersNow-playersBefore).isEqualTo(0);
+        assertThat(exception).isInstanceOf(InvalidStateException.class);
+    }
+
+    @Test
+    public void shouldFailAddPlayerCausedByInvalidGameUuid(){
+        int playersBefore = game.getPlayer().size();
+        Exception exception = null;
+
+        try {
+            gameService.addPlayer("invalid uuid", "player name", false);
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        int playersNow = game.getPlayer().size();
+        assertThat(playersNow-playersBefore).isEqualTo(0);
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     public void shouldStartGame(){
         prepareGame();
 
         gameService.startGame(game.getUuid());
 
         assertStartedGameState();
+    }
+
+    @Test
+    public void shouldFailStartGameCausedByInvalidLifecycle(){
+        prepareGame();
+        game.setGameLifecycle(GameLifecycle.RUNNING);
+        Exception exception = null;
+
+        try {
+            gameService.startGame(game.getUuid());
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertThat(exception).isInstanceOf(InvalidStateException.class);
+        assertThat(game.getTakeStack()).isEmpty();
+    }
+
+    @Test
+    public void shouldFailStartGameCausedByInvalidGameUuid(){
+        prepareGame();
+        game.setGameLifecycle(GameLifecycle.RUNNING);
+        Exception exception = null;
+
+        try {
+            gameService.startGame("invalid");
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+        assertThat(game.getTakeStack()).isEmpty();
     }
 
     private void assertStartedGameState(){
