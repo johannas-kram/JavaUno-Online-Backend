787423e63fe24b97102f57c02f7f608c3a175324
tomatenmark <mh88@mailbox.org>
2019-11-09 13:21:24 +0100

    cleanup

--- src/main/java/de/markherrmann/javauno/data/state/components/GameState.java
+++
@@ -1,5 +0,0 @@
-package de.markherrmann.javauno.data.state.components;
-
-public enum GameState {
-    ADD_PLAYERS, IN_ROUND, BETWEEN_ROUNDS
-}
--- src/main/java/de/markherrmann/javauno/data/state/components/Rounding.java
+++
@@ -1,5 +0,0 @@
-package de.markherrmann.javauno.data.state.components;
-
-public enum Rounding {
-    FREESTYLE, COUNT, POINTS
-}
