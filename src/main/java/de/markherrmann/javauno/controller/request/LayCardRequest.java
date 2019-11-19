15837f07a9dc172e750798a63f56a784c1603ef9
tomatenmark <mh88@mailbox.org>
2019-11-19 21:43:12 +0100

    added controller incl. tests with request class

---
+++ src/main/java/de/markherrmann/javauno/controller/request/LayCardRequest.java
@@ -0,0 +1,43 @@
+package de.markherrmann.javauno.controller.request;
+
+import de.markherrmann.javauno.data.fixed.Card;
+
+public class LayCardRequest {
+
+    private String gameUuid;
+    private String playerUuid;
+    private String cardString;
+    private int cardIndex;
+
+    public String getGameUuid() {
+        return gameUuid;
+    }
+
+    public void setGameUuid(String gameUuid) {
+        this.gameUuid = gameUuid;
+    }
+
+    public String getPlayerUuid() {
+        return playerUuid;
+    }
+
+    public void setPlayerUuid(String playerUuid) {
+        this.playerUuid = playerUuid;
+    }
+
+    public String getCardString() {
+        return cardString;
+    }
+
+    public void setCardString(String cardString) {
+        this.cardString = cardString;
+    }
+
+    public int getCardIndex() {
+        return cardIndex;
+    }
+
+    public void setCardIndex(int cardIndex) {
+        this.cardIndex = cardIndex;
+    }
+}
