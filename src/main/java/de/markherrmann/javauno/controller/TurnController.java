d9e4a5395f17d302bef30b182ff4be2413a1d86b
tomatenmark <mh88@mailbox.org>
2019-11-20 23:27:41 +0100

    deleted unused class

--- src/main/java/de/markherrmann/javauno/controller/TurnController.java
+++
@@ -1,9 +0,0 @@
-package de.markherrmann.javauno.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value= "/turn")
-public class TurnController {
-}
