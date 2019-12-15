174f6e88dd9fe6b108063dd4aec2ac54df19beb1
tomatenmark <mh88@mailbox.org>
2019-12-15 15:56:33 +0100

    improved status codes for GameStateController

--- src/test/java/de/markherrmann/javauno/controller/GameStateControllerTest.java
+++ src/test/java/de/markherrmann/javauno/controller/GameStateControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -73,7 +74,7 @@ public class GameStateControllerTest {
         gameController.startGame(game.getUuid());
 
         MvcResult mvcResult = this.mockMvc.perform(get("/api/gameState/get/{gameUuid}/{playerUuid}", "invalid", player.getUuid()))
-                .andExpect(status().isOk())
+                .andExpect(status().is(HttpStatus.NOT_FOUND.value()))
                 .andReturn();
 
         assertFailure(mvcResult, ExceptionMessage.NO_SUCH_GAME.getValue());
@@ -85,7 +86,7 @@ public class GameStateControllerTest {
         gameController.startGame(game.getUuid());
 
         MvcResult mvcResult = this.mockMvc.perform(get("/api/gameState/get/{gameUuid}/{playerUuid}", game.getUuid(), "invalid"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(HttpStatus.NOT_FOUND.value()))
                 .andReturn();
 
         assertFailure(mvcResult, ExceptionMessage.NO_SUCH_PLAYER.getValue());
