aaae3d4e92eb4dabe7cbedd960a8f1e45ff4ce95
tomatenmark <mh88@mailbox.org>
2020-01-09 22:12:33 +0100

    fix refactoring conflicts 2

---
+++ src/main/java/de/markherrmann/javauno/service/push/PushMessage.java
@@ -0,0 +1,26 @@
+package de.markherrmann.javauno.service.push;
+
+public enum PushMessage {
+
+    STARTED_GAME("started-game"),
+    ADDED_PLAYER("added-player"),
+    REMOVED_PLAYER("removed-player"),
+    PUT_CARD("put-card"),
+    DRAWN_CARD("drawn-card"),
+    KEPT_CARD("kept-card"),
+    SELECTED_COLOR("selected-color"),
+    SAID_UNO("said-uno"),
+    NEXT_TURN("next-turn"),
+    FINISHED_GAME("finished-game"),
+    END("end");
+
+    private final String value;
+
+    PushMessage(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
