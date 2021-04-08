2f9f9389a43f6940bcc3bf1648e8693d565bf475
tomatenmark <mh88@mailbox.org>
2021-04-08 14:32:52 +0200

    separated tests to prevent test monsters

--- src/test/java/de/markherrmann/javauno/service/PlayerServiceAddRemoveTest.java
+++ src/test/java/de/markherrmann/javauno/service/PlayerServiceAddRemoveTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PlayerServiceTest {
+public class PlayerServiceAddRemoveTest {
 
     @Autowired
     private GameService gameService;
@@ -61,24 +61,6 @@ public class PlayerServiceTest {
         assertThat(playersNow).isEqualTo(3);
         assertThat(game.getCurrentPlayerIndex()).isEqualTo(1);
         assertThat(PushService.getLastMessage()).isEqualTo(PushMessage.REMOVED_PLAYER);
-    }
-
-    @Test
-    public void shouldBotifyPlayer(){
-        prepareGame();
-        game.setCurrentPlayerIndex(2);
-        game.setGameLifecycle(GameLifecycle.RUNNING);
-        int playersBefore = game.getPlayers().size();
-
-        playerService.botifyPlayer(game.getUuid(), game.getPlayers().get(1).getUuid());
-        int playersNow = game.getPlayers().size();
-
-        assertThat(playersBefore).isEqualTo(4);
-        assertThat(playersNow).isEqualTo(4);
-        assertThat(game.getCurrentPlayerIndex()).isEqualTo(2);
-        assertThat(PushService.getLastMessage()).isEqualTo(PushMessage.BOTIFIED_PLAYER);
-        assertThat(game.getPlayers().get(1).isBot()).isTrue();
-        assertThat(game.getPlayers().get(1).getBotUuid()).isNotNull();
     }
 
     @Test
@@ -236,52 +218,6 @@ public class PlayerServiceTest {
 
         int playersNow = game.getPlayers().size();
         assertThat(playersNow-playersBefore).isEqualTo(0);
-        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NO_SUCH_PLAYER.getValue());
-    }
-
-    @Test
-    public void shouldFailBotifyPlayerCausedByInvalidLifecycle(){
-        prepareGame();
-        game.setGameLifecycle(GameLifecycle.SET_PLAYERS);
-        Exception exception = null;
-
-        try {
-            playerService.botifyPlayer(game.getUuid(), game.getPlayers().get(0).getUuid());
-        } catch(Exception ex){
-            exception = ex;
-        }
-
-        assertThat(exception).isInstanceOf(IllegalStateException.class);
-        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.INVALID_STATE_GAME.getValue());
-    }
-
-    @Test
-    public void shouldFailBotifyPlayerCausedByInvalidGameUuid(){
-        prepareGame();
-        Exception exception = null;
-
-        try {
-            playerService.removePlayer("invalid uuid", game.getPlayers().get(0).getUuid(), false);
-        } catch(Exception ex){
-            exception = ex;
-        }
-
-        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NO_SUCH_GAME.getValue());
-    }
-
-    @Test
-    public void shouldFailBotifyPlayerCausedByInvalidPlayerUuid(){
-        prepareGame();
-        Exception exception = null;
-
-        try {
-            playerService.removePlayer(game.getUuid(), "invalid uuid",false);
-        } catch(Exception ex){
-            exception = ex;
-        }
-
         assertThat(exception).isInstanceOf(IllegalArgumentException.class);
         assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.NO_SUCH_PLAYER.getValue());
     }
