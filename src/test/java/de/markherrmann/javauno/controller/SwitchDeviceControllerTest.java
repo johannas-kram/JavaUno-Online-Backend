69bbc021da2468bc1d5664e366ada7ead88b2b7c
tomatenmark <mh88@mailbox.org>
2021-04-10 00:29:28 +0200

    fix test

--- src/test/java/de/markherrmann/javauno/controller/SwitchDeviceControllerTest.java
+++ src/test/java/de/markherrmann/javauno/controller/SwitchDeviceControllerTest.java
@@ -1,5 +1,9 @@
 package de.markherrmann.javauno.controller;
 
+import de.markherrmann.javauno.data.state.component.Game;
+import de.markherrmann.javauno.data.state.component.Player;
+import de.markherrmann.javauno.service.GameService;
+import de.markherrmann.javauno.service.PlayerService;
 import de.markherrmann.javauno.service.SwitchDeviceService;
 import de.markherrmann.javauno.service.push.PushMessage;
 import de.markherrmann.javauno.service.push.PushService;
@@ -8,11 +12,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,6 +31,12 @@ public class SwitchDeviceControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private GameService gameService;
+
+    @MockBean
+    private PlayerService playerService;
 
     @Test
     public void shouldSwitchIn() throws Exception {
@@ -40,10 +54,14 @@ public class SwitchDeviceControllerTest {
 
     @Test
     public void shouldSetSwitchFinished() throws Exception {
-        String gameUuid = "testGameUuid";
-        String playerUuid = "testPlayerUuid";
+        Game game = new Game();
+        Player player = new Player("", false);
+        String gameUuid = game.getUuid();
+        String playerUuid = player.getUuid();
+        given(gameService.getGame(gameUuid)).willReturn(game);
+        given(playerService.getPlayer(eq(playerUuid), any(Game.class))).willReturn(player);
 
-        MvcResult mvcResult = this.mockMvc.perform(post("/api/switch/switch-finished/{gameUuid}/{playerUuid}", gameUuid, playerUuid))
+        MvcResult mvcResult = this.mockMvc.perform(post("/api/switch/switch-finished/{gameUuid}/{playerUuid}", game.getUuid(), player.getUuid()))
                 .andExpect(status().isOk())
                 .andReturn();
 
