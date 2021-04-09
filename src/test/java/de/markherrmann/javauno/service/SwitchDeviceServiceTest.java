8d153092b64b1c040119d3ab79d44a1d636fce55
tomatenmark <mh88@mailbox.org>
2021-04-09 21:07:38 +0200

    do not send player uuid to frontend. always use index instead

--- src/test/java/de/markherrmann/javauno/service/SwitchDeviceServiceTest.java
+++ src/test/java/de/markherrmann/javauno/service/SwitchDeviceServiceTest.java
@@ -1,14 +1,20 @@
 package de.markherrmann.javauno.service;
 
+import de.markherrmann.javauno.data.state.component.Game;
+import de.markherrmann.javauno.data.state.component.Player;
 import de.markherrmann.javauno.service.push.PushMessage;
 import de.markherrmann.javauno.service.push.PushService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 
 
 @RunWith(SpringRunner.class)
@@ -17,6 +23,12 @@ public class SwitchDeviceServiceTest {
 
     @Autowired
     SwitchDeviceService switchDeviceService;
+
+    @MockBean
+    private GameService gameService;
+
+    @MockBean
+    private PlayerService playerService;
 
     @Test
     public void shouldSwitchIn(){
@@ -31,8 +43,12 @@ public class SwitchDeviceServiceTest {
 
     @Test
     public void shouldSetSwitchFinished(){
-        String gameUuid = "testGameUuid";
-        String playerUuid = "testPlayerUuid";
+        Game game = new Game();
+        Player player = new Player("", false);
+        String gameUuid = game.getUuid();
+        String playerUuid = player.getUuid();
+        given(gameService.getGame(gameUuid)).willReturn(game);
+        given(playerService.getPlayer(eq(playerUuid), any(Game.class))).willReturn(player);
 
         switchDeviceService.setSwitchFinished(gameUuid, playerUuid);
 
