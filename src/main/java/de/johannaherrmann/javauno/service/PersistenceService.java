a937ee27c572149cfc91f14284ac98097d4ad459
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-05-09 16:39:03 +0200

    feat: prepares red-green-refactor

---
+++ src/main/java/de/johannaherrmann/javauno/service/PersistenceService.java
@@ -0,0 +1,19 @@
+package de.johannaherrmann.javauno.service;
+
+import de.johannaherrmann.javauno.data.state.component.Game;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersistenceService {
+    public void saveGame (Game game) {
+
+    }
+
+    public void deleteGame (String gameUuid) {
+
+    }
+
+    public void loadGames () {
+
+    }
+}
---
+++ src/test/java/de/johannaherrmann/javauno/service/PersistenceServiceTest.java
@@ -0,0 +1,29 @@
+package de.johannaherrmann.javauno.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersistenceServiceTest {
+    @Autowired
+    private PersistenceService persistenceService;
+
+    @Test
+    public void shouldSaveGameCorrectly () {
+
+    }
+
+    @Test
+    public void shouldDeleteGameCorrectly () {
+
+    }
+
+    @Test
+    public void shouldLoadGamesCorrectly () {
+
+    }
+}
